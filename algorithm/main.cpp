7b9e78cb190c8be6e88b1a17421ef2096bb1c586
GiorgioCassata <GiorgioCassata>
2019-02-19 19:11:10 -0700

    Added pseudo for main algorithm

---
+++ algorithm/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // if Next in input  > 2 * last
+    //  do addition
+    // else
+    //  do if (input - last) exists in visited
+    //      then do addiction
+    //     else
+    //  do dfind least greatest visited for its different
+    //      if (difference between remainder and last exists in vistied)
+    //          then do addiction
+    //      else check with visited before last
+    
+
+    return 0;
+}
