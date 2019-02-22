cc05074892f78f9424fd703c1fbe4c95001a7b00
GiorgioCassata <38116994+GiorgioCassata@users.noreply.github.com>
2019-02-22 13:55:28 -0700

    Tidied code problem persists

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -7,6 +7,14 @@
 
 using namespace std;
 
+void printVect(vector<int> a) {
+    cout << endl;
+    for (int z = 0; z < a.size(); ++z) {
+        cout << a.at(z) << endl;
+    }
+    cout << endl;
+}
+
 int main() {
     ifstream input;
     ofstream output;
@@ -31,24 +39,34 @@ int main() {
         unvisited.push_back(tempInt);
     }
 
-    // setup first number (always solves for 2)
+    // setup first number (always solves for 2 using 1's)
     visited.push_back(1);
     lastAddition = 1 + 1; // addition instead of mult for clarification
     visited.push_back(lastAddition);
     output << 1 << ' ' << 1 << endl;
-    cout << lastAddition << endl;
-    i = -1;
+    //cout << lastAddition << endl;
+    i = 0;
     while (visited.back() < unvisited.back()) { // change this condition because we'll take more steps
-        i++;
-        // if next in input > (2 * lastAddition):
+        // prevents skipping numbers
+        if (unvisited.at(i) == lastAddition) {
+            i++;
+            continue;
+        }
+
+        // if the addition of the largest visited number with itself is
+        // less than the target number, it will do that addition.
         if (unvisited.at(i) > 2 * lastAddition) {
-            // do addition
-            visited.push_back(2*lastAddition);
+            output << lastAddition << ' ' << lastAddition << endl;
+            lastAddition = lastAddition + lastAddition;
+            visited.push_back(lastAddition);
+            continue; //continue wihtout iterating because this does not always meet an input
         } else {
-    //     if (input - last) exists in visited
             exists = false;
-            for (int j = 0; j < visited.size(); ++j) {
-                if ((unvisited.at(i) - lastAddition) == visited.at(j)) {
+            for (int j = visited.size()-1; j >= 0; --j) {
+                //cout << visited.at(j) << endl;
+                //cout << (visited.at(j) + lastAddition) << endl;
+                //cout << unvisited.at(i) << endl;
+                if (unvisited.at(i) >= visited.at(j) + lastAddition) {
                     exists = true;
                     indexVis = j;
                     break;
@@ -56,11 +74,20 @@ int main() {
             }
             if (exists) {
                 output << lastAddition << ' ' << visited.at(indexVis) << endl;
-                cout << lastAddition << endl;
+                //cout << lastAddition << endl;
                 lastAddition = lastAddition + visited.at(indexVis);
                 visited.push_back(lastAddition);
             } else {
-                    //         find least greatest visited for its difference
+                output << lastAddition << ' ' << 1 << endl;
+                cout << lastAddition << endl;
+                lastAddition = lastAddition + 1;
+                visited.push_back(lastAddition);
+                cout << "added 1??" << endl;
+                //cout << "not supposed to happen?" << endl;
+                //return 0;
+            }
+            /*else {
+                    //find least greatest visited for its difference
                     exists = false;
                     for (int j = 0; j < visited.size(); ++j) {
                         if ((unvisited.at(i) - visited.at(j)) == lastAddition){
@@ -82,10 +109,11 @@ int main() {
                         visited.push_back(lastAddition);
                     }
 
-            }
+            } */
         }
+        i++; //iterate
     }
-
+    printVect(visited);
     input.close();
     output.close();
     return 0;
