1f148b7459c973bdf6921e7d70bf48ef361c48fd
GiorgioCassata <38116994+GiorgioCassata@users.noreply.github.com>
2019-02-22 13:24:12 -0700

    Converted pseudo to executable code.
    NOTE: There are some problems with our code where it doesnt hit every target sum, but will hit most.

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -8,18 +8,85 @@
 using namespace std;
 
 int main() {
+    ifstream input;
+    ofstream output;
 
-    // if next in input > (2 * lastAddition):
-    //     do addition
-    // else
+    vector<int> unvisited;
+    vector<int> visited;
+
+    bool exists;
+    int indexVis = 0;
+    int lastAddition;
+    int numInputs;
+    int tempInt;
+    int i;
+    input.open("input.txt");
+    output.open("output.txt");
+
+    input >> numInputs;
+
+    // get all inputs into vector
+    while(!input.eof()) {
+        input >> tempInt;
+        unvisited.push_back(tempInt);
+    }
+
+    // setup first number (always solves for 2)
+    visited.push_back(1);
+    lastAddition = 1 + 1; // addition instead of mult for clarification
+    visited.push_back(lastAddition);
+    output << 1 << ' ' << 1 << endl;
+    cout << lastAddition << endl;
+    i = -1;
+    while (visited.back() < unvisited.back()) { // change this condition because we'll take more steps
+        i++;
+        // if next in input > (2 * lastAddition):
+        if (unvisited.at(i) > 2 * lastAddition) {
+            // do addition
+            visited.push_back(2*lastAddition);
+        } else {
     //     if (input - last) exists in visited
-    //         do addition
-    //     else
-    //         find least greatest visited for its difference
-    //          if (difference between remainder and last exists in vistied)
-    //              do addition
-    //          else
-    //             check with visited before last
+            exists = false;
+            for (int j = 0; j < visited.size(); ++j) {
+                if ((unvisited.at(i) - lastAddition) == visited.at(j)) {
+                    exists = true;
+                    indexVis = j;
+                    break;
+                }
+            }
+            if (exists) {
+                output << lastAddition << ' ' << visited.at(indexVis) << endl;
+                cout << lastAddition << endl;
+                lastAddition = lastAddition + visited.at(indexVis);
+                visited.push_back(lastAddition);
+            } else {
+                    //         find least greatest visited for its difference
+                    exists = false;
+                    for (int j = 0; j < visited.size(); ++j) {
+                        if ((unvisited.at(i) - visited.at(j)) == lastAddition){
+                            exists = true;
+                            indexVis = j;
+                            break;
+                        }
+                    }
+                    if (exists) {
+                        output << lastAddition << ' ' << visited.at(indexVis) << endl;
+                        cout << lastAddition << endl;
+                        lastAddition = lastAddition + visited.at(indexVis);
+                        visited.push_back(lastAddition);
+                    }
+                    else {
+                        output << lastAddition << ' ' << 1 << endl;
+                        cout << lastAddition << endl;
+                        lastAddition = lastAddition + 1;
+                        visited.push_back(lastAddition);
+                    }
 
+            }
+        }
+    }
+
+    input.close();
+    output.close();
     return 0;
 }
